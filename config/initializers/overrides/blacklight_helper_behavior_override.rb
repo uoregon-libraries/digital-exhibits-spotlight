a1e63fe93055baa4dc3d4a4da67b32437fcb0d8a
lsat12357 <lsato@uoregon.edu>
2019-09-06 14:25:12 -0700

    display nothing if no title found

--- config/initializers/overrides/blacklight_helper_behavior_override.rb
+++ config/initializers/overrides/blacklight_helper_behavior_override.rb
@@ -1,6 +1,6 @@
 Blacklight::BlacklightHelperBehavior.module_eval do
   def render_document_title_header (document)
-    title = !document["spotlight_hidden_title_tesim"].blank? ? document["spotlight_hidden_title_tesim"].first : "Untitled"
+    title = !document["spotlight_hidden_title_tesim"].blank? ? document["spotlight_hidden_title_tesim"].first : ""
     content_tag('h1', title, itemprop: "name")
   end
 end
