6e560bc5a42d98d8fa0f564b6bb6ab8ae23d9d95
lsat12357 <lsato@uoregon.edu>
2020-12-04 11:14:43 -0800

    docker changes

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,5 +5,6 @@ rm -f tmp/pids/server.pid
 if bundle exec rails db:migrate:status &> /dev/null; then
   bundle exec rails db:migrate
 fi
-
+bundle install
+bundle exec sidekiq -C ./config/sidekiq.yml &
 bundle exec rails s -b 0.0.0.0
