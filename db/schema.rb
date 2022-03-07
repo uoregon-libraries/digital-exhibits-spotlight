a281500db6800808f748b8ac0e4069d8bf047ddc
lsat12357 <lsato@uoregon.edu>
2022-03-07 09:24:56 -0800

    db schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_172440) do
+ActiveRecord::Schema.define(version: 2022_03_01_003208) do
 
   create_table "bookmarks", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
