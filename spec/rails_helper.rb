3ebd8073fc8c5a853e4b06a17caf10af9a9b9b72
lsat12357 <lsato@uoregon.edu>
2022-03-10 07:56:18 -0800

    add require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 require 'rspec/rails'
 require 'spec_helper'
-
+require 'factory_bot_rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
