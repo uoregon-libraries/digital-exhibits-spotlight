54f17094cfb0af19a4c213730869b72d8522663e
lsat12357 <lsato@uoregon.edu>
2023-04-13 06:24:22 -0700

    field_stringifier method

---
+++ app/decorators/field_stringifier.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Another dpul tidbit
+# Spotlight assumes that the title field is the field name, but we need it to be
+# a Field object so it can fall back to override title. This makes it delegate
+# to_s to the field so that autocomplete works.
+class FieldStringifier < SimpleDelegator
+  def to_s
+    __getobj__.field.to_s
+  end
+end
+
