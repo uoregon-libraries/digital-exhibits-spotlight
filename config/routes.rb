bff6e672f172df55bdcda40be821056b8b2543c8
lsat12357 <lsato@uoregon.edu>
2023-12-01 08:37:09 -0800

    remove duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   # Using OmniAuth without other authentications
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     get 'sign_out',  :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
