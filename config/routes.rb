3099658d6aadfa43836e3b5be614d49a1524b7fe
lsat12357 <lsato@uoregon.edu>
2023-12-01 10:10:09 -0800

    remove another duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,6 @@ Rails.application.routes.draw do
 
   # Using OmniAuth without other authentications
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
-  devise_scope :user do
-    get 'sign_out',  :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
 
   concern :exportable, Blacklight::Routes::Exportable.new
 
