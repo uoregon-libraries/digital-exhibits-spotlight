49aee3408251a48d1f55bac91a8c057914cc6e39
Jeremy Echols <jechols@uoregon.edu>
2025-09-29 11:05:58 -0700

    Add script for mirroring prod SQL

---
+++ build/mirror-vars.sh
@@ -0,0 +1,5 @@
+export dev=jechols
+export pod_host=uss-lib-ch1.uoregon.edu
+export pod_subdir=spotlight-upgrade
+export service=db
+export podman_user=podmanuser
---
+++ build/mirror.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Connects to our podman host, using the setup we describe in the
+# "podman-rsync-proxy" project, to run mysqldump commands
+set -eu
+
+source ./build/mirror-vars.sh
+
+echo "Mirroring database structure..."
+ssh $dev@$pod_host sudo -u $podman_user /usr/local/bin/podman-mysqldump.sh $pod_subdir $service \
+    -d spotlight \
+    > ./db-export/001-struct.sql
+echo "Done (structure)."
+
+echo "Mirroring non-guest users..."
+ssh $dev@$pod_host sudo -u $podman_user /usr/local/bin/podman-mysqldump.sh $pod_subdir $service \
+    -t spotlight users '--where="guest = 0"' \
+    > ./db-export/002-users.sql
+echo "Done (users)."
+
+echo "Mirroring core tables..."
+ssh $dev@$pod_host sudo -u $podman_user /usr/local/bin/podman-mysqldump.sh $pod_subdir $service \
+    -t --ignore-table=spotlight.searches --ignore-table=spotlight.users spotlight \
+    > ./db-export/003-core.sql
+echo "Done (core)."
+
+echo "Done."
