d9978138c571d7d6e0af36befacb70d4ceb64dbd
Jeremy Echols <jechols@uoregon.edu>
2025-10-07 07:19:01 -0700

    Re-fix entrypoint
    Ensures bundled gems are installed before database migrations, and that
    assets aren't compiled until *after* migrations. Because somehow Rails
    needs a database connection before it can compile static assets. 🙄

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,9 +4,6 @@
 # something we always want. The migration command will always succeed if there
 # are no migrations to run.
 wait_for_database() {
-  echo "Precompiling assets"
-  rails assets:precompile
-
   echo "Running Rails database migrations"
   i=0
   while [ "$i" -lt 10 ]; do
@@ -19,10 +16,15 @@ wait_for_database() {
   bundle exec rails db:migrate || exit 1
 }
 
-init() {
+bundle_install() {
   echo "Verifying bundled gems are installed"
   rm -f tmp/pids/server.pid
   bundle install
+}
+
+compile_assets() {
+  echo "Precompiling assets"
+  rails assets:precompile
 }
 
 # When user requests bash or sh, don't run the init function
@@ -32,8 +34,9 @@ case "$@" in
   ;;
 
   *)
+  bundle_install
   wait_for_database
-  init
+  compile_assets
   echo "Running bundle exec $@..."
   exec bundle exec "$@"
 esac
